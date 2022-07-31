1a4b1ae8fc1a8451eceb7395be1bd90ec9510c50
alvarenga-allan <allan_alvarenga@msn.com>
2022-07-31 15:07:08 -0300

    chore: add recaptcha

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-const siteKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
-
-function onSubmit(token) {
-  document.getElementById("form").submit();
-}
-
-function onClick(e) {
-  e.preventDefault();
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
-       console.log(token)
-       document.getElementById('g-recaptcha-response').value = token;
-    });
-  });
-}
+let form = document.querySelector('#register');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let name = document.querySelector('#companyName');
+        let email = document.querySelector('#businessEmail');
+        if (!name.value || !email.value) {
+          alert('Nome e Email são Obrigatorios');
+        } else {
+          grecaptcha.execute();
+        }
+      });
+      function sendForm() {
+        alert('reCAPTCHA funcionou');
+      }
