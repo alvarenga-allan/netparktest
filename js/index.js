f704b8a2f382f1d94b049646d7b08d365ded0a02
alvarenga-allan <allan_alvarenga@msn.com>
2022-07-31 20:12:11 -0300

    fix: add correct siteKey

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@ const siteKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 let form = document.querySelector('#register');
       form.addEventListener('submit', (e) => {
         e.preventDefault();
+        grecaptcha.ready(function() {
+          grecaptcha.execute(siteKey, {action: 'submit'}).then(function(token){
+            console.log(recaptcha.token)
+        });
       try{
         let required = document.querySelector('.required')
         if (!required.value){
           throw "Erro 999999 - parâmetros faltantes na chamada"
         }
+        alert("Enviado com sucesso!")
       } catch (error){
-alert("Erro 999999 - parâmetros faltantes na chamada")
+alert("InternalCode: Erro 999999 - parâmetros faltantes na chamada")
       }
     });
-    function sendForm() {
-        alert('Enviado');
-      }
+  });
+
+  console.log(recaptcha.token)
+    // function sendForm() {
+    //     alert('Enviado');
+    //   }
