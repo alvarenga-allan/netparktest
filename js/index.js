eb0973d3bb079d18a25f2e6bdc67fd6e80acb896
alvarenga-allan <allan_alvarenga@msn.com>
2022-07-31 11:34:10 -0300

    chore: adjust recaptcha

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function onSubmit(token) {
 function onClick(e) {
   e.preventDefault();
   grecaptcha.ready(function() {
-    grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'validate_captcha'}).then(function(token) {
+    grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
        console.log(token)
        document.getElementById('g-recaptcha-response').value = token;
     });
