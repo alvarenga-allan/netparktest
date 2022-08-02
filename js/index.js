d44d4e05528985028b284ba5fc9ca78f54db8169
alvarenga-allan <allan_alvarenga@msn.com>
2022-08-02 00:13:17 -0300

    chore: adjust token key and add error 131

--- js/index.js
+++ js/index.js
@@ -1,25 +1,40 @@
-const siteKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-
+const siteKey = "6LcDdjYhAAAAAH5UsKV7M652mrxd9-p3T9dk_yCA"
 let form = document.querySelector('#register');
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        grecaptcha.ready(function() {
-          grecaptcha.execute(siteKey, {action: 'submit'}).then(function(token){
-            console.log(recaptcha.token)
-        });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    grecaptcha.ready(function() {
+        grecaptcha.execute(siteKey, {action: 'submit'}).then(function(token) {
+          
+            const gRecaptchaResponse=document.querySelector("#g-recaptcha-response").value=token;
+        
+   
+      
       try{
         let required = document.querySelector('.required')
         if (!required.value){
           throw "Erro 999999 - parâmetros faltantes na chamada"
         }
-        alert("Enviado com sucesso!")
+        alert("Orçamento enviado com sucesso!")
+        
       } catch (error){
-alert("InternalCode: Erro 999999 - parâmetros faltantes na chamada")
-      }
-    });
-  });
+        alert("InternalCode: Erro 999999 - parâmetros faltantes na chamada")
 
-  console.log(recaptcha.token)
-    // function sendForm() {
-    //     alert('Enviado');
-    //   }
+      }
+    
+      
+      try{
+      
+        if (gRecaptchaResponse.length===0){
+            throw "Erro 131 - erro no Google Recaptcha"
+        }
+        alert("Token gerado com sucesso")
+        
+      } catch (error){
+        alert("Erro 131 - erro no Google Recaptcha")
+
+      }
+    
+});
+});
+});
